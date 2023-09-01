b09572b679fa3f31fbfe0903f5bb7af0e8fb592c
ruitianzhong <ruitian-zhong@outlook.com>
2023-09-01 13:40:07 +0800

    server side code without applying the Op

--- src/shardctrler/server.go
+++ src/shardctrler/server.go
@@ -1,11 +1,15 @@
 package shardctrler
 
+import (
+	"sync"
+	"time"
 
-import "6.5840/raft"
-import "6.5840/labrpc"
-import "sync"
-import "6.5840/labgob"
+	"6.5840/labgob"
+	"6.5840/labrpc"
+	"6.5840/raft"
+)
 
+const wait = 1 * time.Millisecond
 
 type ShardCtrler struct {
 	mu      sync.Mutex
@@ -16,30 +20,168 @@ type ShardCtrler struct {
 	// Your data here.
 
 	configs []Config // indexed by config num
-}
 
+	dup map[int64]CtrlerReply
+}
+type CtrlerOpType string
+
+const (
+	MOVE  CtrlerOpType = "Move"
+	JOIN  CtrlerOpType = "Join"
+	LEAVE CtrlerOpType = "Leave"
+	QUERY CtrlerOpType = "Query"
+)
 
 type Op struct {
 	// Your data here.
+	ClientId     int64
+	SeqNumber    int
+	CtrlerOpType CtrlerOpType
+	JoinArgs     JoinArgs
+	LeaveArgs    LeaveArgs
+	MoveArgs     MoveArgs
+	QueryArgs    QueryArgs
 }
 
+type CtrlerReply struct {
+	SeqNumber    int
+	CtrlerOpType CtrlerOpType
+	JoinReply    JoinReply
+	LeaveReply   LeaveReply
+	MoveReply    MoveReply
+	QueryReply   QueryReply
+}
 
 func (sc *ShardCtrler) Join(args *JoinArgs, reply *JoinReply) {
 	// Your code here.
+	op := Op{ClientId: args.ClientId, SeqNumber: args.SeqNumber, CtrlerOpType: JOIN, JoinArgs: *args}
+	_, term, isLeader := sc.rf.Start(op)
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+	for {
+		currentTerm, _ := sc.rf.GetState()
+		if currentTerm != term {
+			reply.WrongLeader = true
+			return
+		}
+		sc.mu.Lock()
+		v, ok := sc.dup[args.ClientId]
+		if ok {
+			if args.SeqNumber == v.SeqNumber {
+				*reply = v.JoinReply
+				sc.mu.Unlock()
+				return
+			} else if args.SeqNumber < v.SeqNumber {
+				reply.WrongLeader = true
+				sc.mu.Unlock()
+				return
+			}
+		}
+		sc.mu.Unlock()
+		time.Sleep(wait)
+	}
+
 }
 
 func (sc *ShardCtrler) Leave(args *LeaveArgs, reply *LeaveReply) {
 	// Your code here.
+	op := Op{ClientId: args.ClientId, SeqNumber: args.SeqNumber, CtrlerOpType: LEAVE, LeaveArgs: *args}
+	_, term, isLeader := sc.rf.Start(op)
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+	for {
+		currentTerm, _ := sc.rf.GetState()
+		if currentTerm != term {
+			reply.WrongLeader = true
+			return
+		}
+		sc.mu.Lock()
+		v, ok := sc.dup[args.ClientId]
+		if ok {
+			if args.SeqNumber == v.SeqNumber {
+				*reply = v.LeaveReply
+				sc.mu.Unlock()
+				return
+			} else if args.SeqNumber < v.SeqNumber {
+				reply.WrongLeader = true
+				sc.mu.Unlock()
+				return
+			}
+		}
+		sc.mu.Unlock()
+		time.Sleep(wait)
+	}
+
 }
 
 func (sc *ShardCtrler) Move(args *MoveArgs, reply *MoveReply) {
 	// Your code here.
+	op := Op{ClientId: args.ClientId, SeqNumber: args.SeqNumber, CtrlerOpType: MOVE, MoveArgs: *args}
+	_, term, isLeader := sc.rf.Start(op)
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+	for {
+		currentTerm, _ := sc.rf.GetState()
+		if currentTerm != term {
+			reply.WrongLeader = true
+			return
+		}
+		sc.mu.Lock()
+		v, ok := sc.dup[args.ClientId]
+		if ok {
+			if args.SeqNumber == v.SeqNumber {
+				*reply = v.MoveReply
+				sc.mu.Unlock()
+				return
+			} else if args.SeqNumber < v.SeqNumber {
+				reply.WrongLeader = true
+				sc.mu.Unlock()
+				return
+			}
+		}
+		sc.mu.Unlock()
+		time.Sleep(wait)
+	}
+
 }
 
 func (sc *ShardCtrler) Query(args *QueryArgs, reply *QueryReply) {
+	op := Op{ClientId: args.ClientId, SeqNumber: args.SeqNumber, CtrlerOpType: QUERY, QueryArgs: *args}
+	_, term, isLeader := sc.rf.Start(op)
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+	for {
+		currentTerm, _ := sc.rf.GetState()
+		if currentTerm != term {
+			reply.WrongLeader = true
+			return
+		}
+		sc.mu.Lock()
+		v, ok := sc.dup[args.ClientId]
+		if ok {
+			if args.SeqNumber == v.SeqNumber {
+				*reply = v.QueryReply
+				sc.mu.Unlock()
+				return
+			} else if args.SeqNumber < v.SeqNumber {
+				reply.WrongLeader = true
+				sc.mu.Unlock()
+				return
+			}
+		}
+		sc.mu.Unlock()
+		time.Sleep(wait)
+	}
 	// Your code here.
 }
-
 
 // the tester calls Kill() when a ShardCtrler instance won't
 // be needed again. you are not required to do anything
@@ -71,6 +213,55 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister)
 	sc.rf = raft.Make(servers, me, persister, sc.applyCh)
 
 	// Your code here.
-
+	go sc.rxMsg()
 	return sc
+}
+
+func (sc *ShardCtrler) rxMsg() {
+
+	for msg := range sc.applyCh {
+		sc.mu.Lock()
+
+		if msg.CommandValid {
+			command, ok := msg.Command.(Op)
+			if !ok {
+				panic("Unexpected command type")
+			}
+			v, exist := sc.dup[command.ClientId]
+			if !exist || v.SeqNumber < command.SeqNumber {
+				switch command.CtrlerOpType {
+				case JOIN:
+					sc.applyJoin(command)
+				case LEAVE:
+					sc.applyLeave(command)
+				case MOVE:
+					sc.applyMove(command)
+				case QUERY:
+					sc.applyQuery(command)
+				default:
+					panic("Unexpected CtrlerOpType")
+				}
+			}
+		} else {
+			panic("Unexpected applyMsg type")
+		}
+
+		sc.mu.Unlock()
+	}
+}
+
+func (sc *ShardCtrler) applyJoin(op Op) {
+
+}
+
+func (sc *ShardCtrler) applyLeave(op Op) {
+
+}
+
+func (sc *ShardCtrler) applyQuery(op Op) {
+
+}
+
+func (sc *ShardCtrler) applyMove(op Op) {
+
 }
