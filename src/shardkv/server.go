79f65a336acba6402137ef0f8e4bc395e7743bea
ruitianzhong <ruitian-zhong@outlook.com>
2023-09-02 21:34:17 +0800

    implement shard migration

--- src/shardkv/server.go
+++ src/shardkv/server.go
@@ -25,9 +25,11 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 type OpType string
 
 const (
-	PUT    OpType = "Put"
-	GET    OpType = "Get"
-	APPEND OpType = "Append"
+	PUT          OpType = "Put"
+	GET          OpType = "Get"
+	APPEND       OpType = "Append"
+	CONFIG       OpType = "Config"
+	InstallShard OpType = "InstallShard"
 )
 
 type Op struct {
@@ -39,6 +41,9 @@ type Op struct {
 	Get       GetArgs
 	ClientId  int64
 	SeqNum    int
+
+	Config       shardctrler.Config
+	InstallShard InstallShardArgs
 }
 
 type CachedReply struct {
@@ -58,19 +63,47 @@ type ShardKV struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
-	kvMap    map[string]string
-	dup      map[int64]CachedReply
-	shardMap map[int]map[string]string
-
+	kvMap          map[string]string
+	dup            map[int64]CachedReply
+	mp             map[ShardKey]Shard
 	lastApplyIndex int
 	persister      *raft.Persister
 	dead           int32
 	clerk          *shardctrler.Clerk
 	config         shardctrler.Config
+
+	curConfigNum [shardctrler.NShards]int
+}
+
+type Shard struct {
+	KVMap  map[string]string
+	Result map[int64]CachedReply
+	Mark   bool
+}
+
+type ShardKey struct {
+	Shard, Num int
+}
+
+type InstallShardArgs struct {
+	ShardKey ShardKey
+	Shard    Shard
+}
+
+type InstallShardReply struct {
+	Err Err
 }
 
 func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	if kv.checkKey(args.Key) {
+		reply.Err = ErrWrongGroup
+		kv.mu.Unlock()
+		return
+	}
+	kv.mu.Unlock()
+
 	op := Op{OpType: GET, Get: *args, ClientId: args.ClientId, SeqNum: args.SeqNum}
 	_, term, isLeader := kv.rf.Start(op)
 	if !isLeader {
@@ -105,6 +138,13 @@ func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) {
 }
 
 func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
+	kv.mu.Lock() // quickly return ErrWrongGroup
+	if kv.checkKey(args.Key) {
+		reply.Err = ErrWrongGroup
+		kv.mu.Unlock()
+		return
+	}
+	kv.mu.Unlock()
 	// Your code here.
 	op := Op{OpType: OpType(args.Op), PutAppend: *args, ClientId: args.ClientId, SeqNum: args.SeqNum}
 	_, term, isLeader := kv.rf.Start(op)
@@ -200,7 +240,6 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister,
 	kv.config.Groups = make(map[int][]string)
 
 	kv.kvMap = make(map[string]string)
-	kv.shardMap = make(map[int]map[string]string)
 	kv.dup = make(map[int64]CachedReply)
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
@@ -223,13 +262,20 @@ func (kv *ShardKV) rxMsg() {
 				panic("Unexpected type")
 			}
 			kv.mu.Lock()
-			id, seq := command.ClientId, command.SeqNum
-			v, ok := kv.dup[id]
-			if !ok || v.SeqNum < seq {
-				reply := new(CachedReply)
-				kv.applyCommand(reply, command)
-				kv.dup[id] = *reply
+			if command.OpType == GET || command.OpType == PUT || command.OpType == APPEND {
+				id, seq := command.ClientId, command.SeqNum
+				v, ok := kv.dup[id]
+				if !ok || v.SeqNum < seq {
+					reply := new(CachedReply)
+					kv.applyCommand(reply, command)
+					kv.dup[id] = *reply
+				}
+			} else if command.OpType == CONFIG {
+				kv.applyConfigCommand(command)
+			} else if command.OpType == InstallShard {
+				kv.applyInstallShardCommand(command)
 			}
+
 			DPrintf("ShardServer %v apply index:%v command:%v", kv.me, msg.CommandIndex, msg.Command)
 			kv.lastApplyIndex = msg.CommandIndex
 			kv.mu.Unlock()
@@ -252,6 +298,10 @@ func (kv *ShardKV) applyCommand(reply *CachedReply, op Op) {
 	reply.Type = op.OpType
 	reply.SeqNum = op.SeqNum
 	if op.OpType == PUT || op.OpType == APPEND {
+		if kv.checkKey(op.PutAppend.Key) {
+			reply.PutAppendReply.Err = ErrWrongGroup
+			return
+		}
 
 		v, ok := kv.kvMap[op.PutAppend.Key]
 		if ok && op.OpType == APPEND {
@@ -264,6 +314,10 @@ func (kv *ShardKV) applyCommand(reply *CachedReply, op Op) {
 		reply.PutAppendReply.Err = OK
 
 	} else if op.OpType == GET {
+		if kv.checkKey(op.Get.Key) {
+			reply.GetReply.Err = ErrWrongGroup
+			return
+		}
 		v, ok := kv.kvMap[op.Get.Key]
 		if ok {
 			reply.GetReply.Value = v
@@ -275,6 +329,53 @@ func (kv *ShardKV) applyCommand(reply *CachedReply, op Op) {
 		panic(op.OpType)
 	}
 
+}
+
+func (kv *ShardKV) checkKey(key string) bool {
+	shard := key2shard(key)
+	return kv.config.Shards[shard] != kv.me
+}
+
+func (kv *ShardKV) applyConfigCommand(op Op) {
+	if kv.config.Num < op.Config.Num {
+		oc, nc := kv.config, op.Config
+		for i := 0; i < shardctrler.NShards; i++ {
+			if oc.Shards[i] == kv.me && nc.Shards[i] != kv.me {
+				go kv.sendInstallShard(i, nc.Num, nc.Shards[i])
+			}
+		}
+
+		kv.config = op.Config
+
+	}
+}
+func (kv *ShardKV) applyInstallShardCommand(op Op) {
+	kv.mu.Lock()
+	if kv.curConfigNum[op.InstallShard.ShardKey.Shard] < op.InstallShard.ShardKey.Num {
+		kv.curConfigNum[op.InstallShard.ShardKey.Shard] = op.InstallShard.ShardKey.Num
+		kv.mp[op.InstallShard.ShardKey] = op.InstallShard.Shard
+	}
+	kv.mu.Unlock()
+}
+func (kv *ShardKV) sendInstallShard(shard, config, gid int) {
+	for {
+		if servers, ok := kv.config.Groups[gid]; ok {
+			for si := 0; si < len(servers); si++ {
+				srv := kv.make_end(servers[si])
+				var reply InstallShardReply
+				args := InstallShardArgs{}
+				args.ShardKey = ShardKey{Shard: shard, Num: config}
+				kv.mu.Lock()
+				args.Shard = kv.mp[args.ShardKey]
+				kv.mu.Unlock()
+				ok := srv.Call("ShardKV.InstallShard", &args, &reply)
+				if ok && reply.Err == OK {
+					return
+				}
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func (kv *ShardKV) encodeSnapshot() []byte {
@@ -317,4 +418,64 @@ func (kv *ShardKV) initSnapshot() {
 	if len(snapshot) > 0 {
 		kv.decodeSnapshot(snapshot)
 	}
+}
+
+func (kv *ShardKV) updateConfig() {
+	for !kv.killed() {
+		newConfig := kv.clerk.Query(kv.config.Num + 1)
+		oldConfig := kv.config
+		kv.mu.Lock()
+		if oldConfig.Num == newConfig.Num {
+			kv.mu.Unlock()
+			time.Sleep(500 * time.Millisecond)
+			continue
+		}
+		kv.mu.Unlock()
+
+		args := Op{OpType: CONFIG, Config: newConfig}
+		for {
+			_, _, isLeader := kv.rf.Start(args)
+			if !isLeader {
+				break
+			}
+			time.Sleep(30 * time.Millisecond)
+			kv.mu.Lock()
+			defer kv.mu.Unlock()
+			if kv.config.Num == newConfig.Num {
+				break
+			}
+		}
+	}
+
+}
+
+func (kv *ShardKV) InstallShard(args *InstallShardArgs, reply *InstallShardReply) {
+
+	kv.mu.Lock()
+
+	if args.ShardKey.Num <= kv.curConfigNum[args.ShardKey.Shard] {
+		kv.mu.Unlock()
+		reply.Err = OK
+		return
+	}
+	kv.mu.Unlock()
+	op := Op{}
+	op.OpType = InstallShard
+	op.InstallShard = *args
+	for {
+		_, _, isLeader := kv.rf.Start(op)
+		if !isLeader {
+			reply.Err = ErrWrongLeader
+			return
+		}
+		time.Sleep(50 * time.Millisecond)
+		kv.mu.Lock()
+		if args.ShardKey.Num <= kv.curConfigNum[args.ShardKey.Shard] {
+			kv.mu.Unlock()
+			reply.Err = OK
+			return
+		}
+		kv.mu.Unlock()
+	}
+
 }
